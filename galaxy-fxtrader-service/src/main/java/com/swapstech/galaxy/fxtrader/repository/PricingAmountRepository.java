e2885315f75c9a87a18dc5942d56f911ba59a0f0
ManikandanFinzly <manikandan.s@finzly.com>
2024-05-27 10:22:03 +0530

    FX-trader -> Service Changes...

--- galaxy-fxtrader-service/src/main/java/com/swapstech/galaxy/fxtrader/repository/PricingAmountRepository.java
+++ galaxy-fxtrader-service/src/main/java/com/swapstech/galaxy/fxtrader/repository/PricingAmountRepository.java
@@ -1,23 +1,16 @@
 package com.swapstech.galaxy.fxtrader.repository;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.swapstech.galaxy.fxtrader.model.PricingAmount;
-import com.swapstech.galaxy.fxtrader.model.PricingTier;
 
 @Repository
 public interface PricingAmountRepository extends JpaRepository<PricingAmount, UUID>, JpaSpecificationExecutor<PricingAmount> {
 
-	public PricingTier findByName( String name);
-	
-	@Query(nativeQuery = true, value = "SELECT pt.id, pt.name, pt.is_default FROM pricing_tier pt INNER JOIN pricing_item pti ON pt.id = pti.pricing_tier_id WHERE pti.tier_type = :tierType")
-	public List<Object[]> getAllTiers(@Param("tierType") int tierType);
+	public PricingAmount findByName( String name);
 
 }
